c8227a07fbf1ffa4ed73499f9ec22cf59e3708e2
jbuggea <jodiebuggea@me.com>
2017-09-29 11:25:56 +0100

    Add files via upload

--- exerciseOne.js
+++ exerciseOne.js
@@ -1,5 +1,37 @@
-var date = new Date();
-var n = date.toDateString();
-var time = date.toLocaleTimeString();
+var today = new Date();
+  var day = today.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + ".");
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var timeofday = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && timeofday===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  timeofday=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  timeofday=' PM';
+  } 
+  } 
+  if (hour===0 && timeofday===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  timeofday=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  timeofday=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + timeofday + " : " + minute + " : " + second);
 
-console.log(n + ' ' + time);
