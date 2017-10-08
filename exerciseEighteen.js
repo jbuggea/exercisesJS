b054b84a2c21adf1ad71dd6cbf8ed48e856c03fb
jbuggea <jodiebuggea@me.com>
2017-10-08 10:17:43 +0100

    Delete exerciseEighteen.js

--- exerciseEighteen.js
+++
@@ -1,8 +0,0 @@
-function testnum(x, y) 
-{
-  return ((x == 50 || y == 50) || (x + y == 50));
-}
-console.log(testnum(50, 50))
-console.log(testnum(20, 50))
-console.log(testnum(20, 20))
-console.log(testnum(20, 30))
